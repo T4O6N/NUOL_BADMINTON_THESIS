9db84199ad5d529d66a4f454941f824552eb9a4b
Vilaphon Bounmachanh <justphonn@Vilaphons-MacBook-Pro.local>
2024-07-21 18:20:22 +0700

    commit setup firebase

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,14 @@ import 'package:get/get.dart';
 import 'package:nuol_badminton_thesis/app/modules/payment_detail/views/payment_detail_view.dart';
 
 import 'app/routes/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
